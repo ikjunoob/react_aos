da2e3ed5e899f3b8a5883d33de860eaeb18d1f9b
KIJ <dlrwn0730@naver.com>
2025-08-07 15:03:59 +0900

    ㅣ

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../style/Hero.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 function Hero() {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            offset: 120
+        });
+    })
     return (
-        <Hero>Hero</Hero>
+        <section className='Hero'>
+            <div className="t=wrap">
+                <h2 data-aos='fade-up'>Hero Title</h2>
+                <p data-aos='fade-up' data-aos-delay="100">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, architecto?
+                </p>
+            </div>
+        </section>
     )
 }
 
